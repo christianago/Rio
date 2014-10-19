bd6535d7c92c21d75279cf3bdcd0196d0a76a883
Kris <christakhs@live.com>
2014-10-19 12:48:15 +0300

    First

---
+++ js/index.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+	$('input.datepicker').datepicker({
+		autoclose: true,
+		startDate: '+1d'
+	});
+	
+	
+	$(this).on('mouseenter', '.datepicker-switch', function(){
+		$(this).css('background', '#DC143C');
+	});
+
+	 $(this).on('click', '.dropdown-menu *', function(e){
+	     e.stopPropagation();
+	 });
+	 
+	 $(this).on('focus', 'input', function(){
+		 if ( $('.datepicker').length > 0 )
+		 $('input.datepicker').datepicker('hide');
+	 });
+	 
+	 var options = { $AutoPlay:true, $SlideshowOptions: { $Class: $JssorSlideshowRunner$, $Transitions: [{ $Duration:700, $Fade: true, $Opacity:2 }] }};
+     var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+     
+
+     function ScaleSlider() {
+         var parentWidth = $('#slider1_container').parent().width();
+         if (parentWidth) {
+             jssor_slider1.$ScaleWidth(parentWidth);
+         }
+         else
+             window.setTimeout(ScaleSlider, 30);
+     }
+     //Scale slider after document ready
+     ScaleSlider();
+     if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
+         //Capture window resize event
+         $(window).bind('resize', ScaleSlider);
+     }
+
+     
+    //$('#slider1_container').css({ height: 100 });
+    //jssor_slider1.$ScaleWidth(1600);
+    
+    
+    
+});
+	
+
+function getToday(){
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1;
+	var yyyy = today.getFullYear();
+	if(dd<10) {
+	    dd='0'+dd;
+	} 
+	today = mm+'/'+dd+'/'+yyyy;
+	if(mm<10) {
+	    mm='0'+mm;
+	} 
+	return today;
+}
+
+
